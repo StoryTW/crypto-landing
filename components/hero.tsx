e24f428d16411daeb349938e3be51ebe47ca123a
ParienkoSA <parienkosa@abdgtl.com>
2025-06-03 01:02:31 +0300

    fix

--- components/hero.tsx
+++ components/hero.tsx
@@ -199,10 +199,12 @@ export default function Hero() {
                     <MoveRight className="ml-2 h-4 w-4 transition-transform group-hover:translate-x-1" />
                   </Button>
                 </DialogTrigger>
-                <Button size="lg" variant="outline" className="group">
-                  Get Trading App
-                  <Download className="ml-2 h-4 w-4 transition-transform group-hover:translate-y-1" />
-                </Button>
+                <a href="http://localhost:5050" target="_blank">
+                  <Button size="lg" variant="outline" className="group">
+                    Get Trading App
+                    <Download className="ml-2 h-4 w-4 transition-transform group-hover:translate-y-1" />
+                  </Button>
+                </a>
               </div>
               <DialogContent className="sm:max-w-[425px]">
                 <DialogHeader>
